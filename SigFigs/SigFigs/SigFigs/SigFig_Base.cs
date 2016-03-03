266d76e2c7fbc0c2a56bfe60e19c1f6913cab1ee
exlted <exltedofdrg@gmail.com>
2016-03-03 12:07:16 -0500

    added .Add() command for testing purposes

--- SigFigs/SigFigs/SigFigs/SigFig_Base.cs
+++ SigFigs/SigFigs/SigFigs/SigFig_Base.cs
@@ -17,6 +17,12 @@ namespace SigFigs.SigFigs
         /// </summary>
         short trailingZeroes;
 
+        public SigFig(short value)
+        {
+            sigFigs = value;
+            trailingZeroes = 0;
+        }
+
         public SigFig(int value)
         {
             if (value < 999999999 && value > -999999999)
@@ -33,7 +39,61 @@ namespace SigFigs.SigFigs
 
         public SigFig(long value)
         {
+            if(value < 999999999 && value > -999999999)
+            {
+                sigFigs = (int)value;
+                trailingZeroes = 0;
+            }
+            else if(value < 9999999999 && value > -9999999999)
+            {
+                sigFigs = (int)(value / 10);
+                trailingZeroes = 1;
+            }
+            else if(value < 99999999999 && value > -99999999999)
+            {
+                sigFigs = (int)(value / 100);
+                trailingZeroes = 2;
+            }
+            else if(value < 999999999999 && value > -999999999999)
+            {
+                sigFigs = (int)(value / 1000);
+                trailingZeroes = 3;
+            }
+            else if(value < 9999999999999 && value > -9999999999999)
+            {
+                sigFigs = (int)(value / 10000);
+                trailingZeroes = 4;
+            }
+            else if(value < 99999999999999 && value > -99999999999999)
+            {
+                sigFigs = (int)(value / 100000);
+                trailingZeroes = 5;
+            }
+            else if(value < 999999999999999 && value > -999999999999999)
+            {
+                sigFigs = (int)(value / 1000000);
+                trailingZeroes = 6;
+            }
+            else if(value < 9999999999999999 && value > -9999999999999999)
+            {
+                sigFigs = (int)(value / 10000000);
+                trailingZeroes = 7;
+            }
+            else if(value < 99999999999999999 && value > -99999999999999999)
+            {
+                sigFigs = (int)(value / 100000000);
+                trailingZeroes = 8;
+            }
+            else if(value < 999999999999999999 && value > -999999999999999999)
+            {
+                sigFigs = (int)(value / 1000000000);
+                trailingZeroes = 9;
+            }
+        }
 
+        public static implicit operator SigFig (short value)
+        {
+            return new SigFig(value);
         }
 
         public static implicit operator SigFig (int value)
@@ -44,6 +104,61 @@ namespace SigFigs.SigFigs
         public static implicit operator SigFig (long value)
         {
             return new SigFig(value);
+        }
+
+        /// <summary>
+        /// Adds the specified second.
+        /// </summary>
+        /// <param name="second">The second.</param>
+        /// <returns></returns>
+        public SigFig Add(SigFig second)
+        {
+            long temp = 0;
+            if (trailingZeroes > second.trailingZeroes)
+            {
+                if ((this.trailingZeroes -= second.trailingZeroes) > 9)
+                    return this;
+                temp = this.sigFigs + (second.sigFigs / 10 ^ (this.trailingZeroes - second.trailingZeroes));
+                if (temp > 999999999 || temp < -999999999)
+                {
+                    this.sigFigs = (int)temp / 10;
+                    this.trailingZeroes += 1;
+                }
+                else
+                {
+                    this.sigFigs = (int)temp;
+                }
+            }
+            else if (second.trailingZeroes > this.trailingZeroes)
+            {
+                if ((second.trailingZeroes -= this.trailingZeroes) > 9)
+                    return second;
+                temp = second.sigFigs + (this.sigFigs / 10 ^ (second.trailingZeroes - this.trailingZeroes));
+                if (temp > 999999999 || temp < -999999999)
+                {
+                    this.sigFigs = (int)temp / 10;
+                    this.trailingZeroes = second.trailingZeroes++;
+                }
+                else
+                {
+                    this.sigFigs = (int)temp;
+                    this.trailingZeroes = second.trailingZeroes;
+                }
+            }
+            else
+            {
+                temp = this.sigFigs + second.sigFigs;
+                if (temp > 999999999 || temp < -999999999)
+                {
+                    this.sigFigs = (int)temp / 10;
+                    this.trailingZeroes += 1;
+                }
+                else
+                {
+                    this.sigFigs = (int)temp;
+                }
+            }
+            return this;
         }
 
         public static SigFig operator +(SigFig first, SigFig second)
@@ -82,13 +197,19 @@ namespace SigFigs.SigFigs
             }
             else
             {
-
-
+                temp = first.sigFigs + second.sigFigs;
+                if (temp > 999999999 || temp < -999999999)
+                {
+                    first.sigFigs = (int)temp / 10;
+                    first.trailingZeroes += 1;
+                }
+                else
+                {
+                    first.sigFigs = (int)temp;
+                }
             }
             return first;
         }
-
-
 
         /// <summary>
         /// Returns a <see cref="System.String" /> that represents this instance.
@@ -108,5 +229,19 @@ namespace SigFigs.SigFigs
             }
             return temp.ToString();
         }
+
+        /// <summary>
+        /// Gets the value of a number up to the number of specified significant figures.
+        /// </summary>
+        /// <param name="numberOfFigs">The number of figures to return.</param>
+        /// <returns></returns>
+        //int getSigFigs(int numberOfFigs)
+        //{
+        //    if(numberOfFigs >= 9)
+        //    {
+        //        return sigFigs;
+        //    }
+
+        //}
     }
 }
