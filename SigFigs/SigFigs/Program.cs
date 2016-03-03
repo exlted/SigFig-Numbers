56a5749cb6b96936398afa94b2ed01380312da3d
Trevor Lory <trevorlory@att.net>
2016-03-03 12:39:56 -0500

    Added more Test cases

--- SigFigs/SigFigs/Program.cs
+++ SigFigs/SigFigs/Program.cs
@@ -12,7 +12,7 @@ namespace SigFigs
         static void Main(string[] args)
         {
             //Test 1
-            SigFig x = 999999999999990;
+            SigFig x = int.MaxValue;
             Console.WriteLine("Base Num: " + x.ToString());
             Console.WriteLine("Base Num: " + x.ToString(true));
             x = x + x;
@@ -20,10 +20,20 @@ namespace SigFigs
             Console.WriteLine("Num after addition: " + x.ToString(true));
 
             //Test 2
-            SigFig a = (short)1200;
-            Console.WriteLine("\nBase Num: " + a.ToString());
+            x = short.MaxValue;
+            Console.WriteLine("\nBase Num: " + x.ToString());
+            Console.WriteLine("Base Num: " + x.ToString(true));
+            x = x + x;
+            Console.WriteLine("Num after addition: " + x.ToString());
+            Console.WriteLine("Num after addition: " + x.ToString(true));
 
-
+            //Test 3
+            x = long.MaxValue;
+            Console.WriteLine("\nBase Num: " + x.ToString());
+            Console.WriteLine("Base Num: " + x.ToString(true));
+            x = x + x;
+            Console.WriteLine("Num after addition: " + x.ToString());
+            Console.WriteLine("Num after addition: " + x.ToString(true));
 
             Console.SetCursorPosition(0, Console.WindowHeight - 1);
             Console.Write("Press ENTER to continue...");
